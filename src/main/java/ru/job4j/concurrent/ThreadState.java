4375032a40695b6e21901a12e354de69a7c50021
Pozhar <abrakatabr@mail.ru>
2025-07-13 15:08:26 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,16 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.isAlive() || second.isAlive()) { }
+        System.out.println("Program completed");
+    }
+}
