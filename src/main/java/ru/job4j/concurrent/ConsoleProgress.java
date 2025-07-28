7583863e2e4c094a0ed8e96c3fdb418f1ddf24a2
Pozhar <abrakatabr@mail.ru>
2025-07-28 21:53:39 +0300

    6. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -11,7 +11,6 @@ public class ConsoleProgress implements Runnable {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                break;
             }
             System.out.print("\r load: " + process[index++ % 4]);
         }
